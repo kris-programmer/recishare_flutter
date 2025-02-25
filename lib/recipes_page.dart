d50df4542b905b7ebc1ad2a937d225d108bef419
KrisB <kris.formal@gmail.com>
2025-02-25 15:27:39 +0200

    Gradle fixes

--- lib/recipes_page.dart
+++ lib/recipes_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:recishare_flutter/recipe.dart';
 import 'package:recishare_flutter/recipe_list.dart';
-import 'package:recishare_flutter/settings_page.dart';
 
 class RecipesPage extends StatefulWidget {
   const RecipesPage({super.key});
@@ -25,8 +24,7 @@ class _RecipesPageState extends State<RecipesPage> {
     return Scaffold(
         body: Expanded(child: RecipeList(recipes)),
         floatingActionButton: FloatingActionButton(
-            onPressed: () => Navigator.push(context,
-                MaterialPageRoute(builder: (context) => const SettingsPage())),
+            onPressed: () => newRecipe(),
             backgroundColor: Colors.green,
             child: const Icon(Icons.add)));
   }
