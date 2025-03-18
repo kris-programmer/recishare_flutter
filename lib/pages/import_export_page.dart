ead7446009c8c99f6e96556fd3944b1fcc159293
KrisB <kris.formal@gmail.com>
2025-03-18 15:54:37 +0200

    - Added the permission_handler dependency and implemented external storage permissions for import/export functionality

--- lib/pages/import_export_page.dart
+++ lib/pages/import_export_page.dart
@@ -4,6 +4,7 @@ import 'dart:io';
 import 'dart:typed_data';
 import 'package:recishare_flutter/recipe.dart';
 import 'package:recishare_flutter/recipe_service.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class ImportExportPage extends StatefulWidget {
   final Function() onRefresh;
@@ -18,6 +19,9 @@ class _ImportExportPageState extends State<ImportExportPage> {
   final RecipeService _recipeService = RecipeService();
 
   Future<void> _importRecipes() async {
+    // Clear the cache before importing a file
+    await FilePicker.platform.clearTemporaryFiles();
+
     FilePickerResult? result = await FilePicker.platform.pickFiles(
       type: FileType.custom,
       allowedExtensions: ['json'],
@@ -26,7 +30,9 @@ class _ImportExportPageState extends State<ImportExportPage> {
     if (result != null) {
       String filePath = result.files.single.path!;
       String fileContent = await File(filePath).readAsString();
+
       List<Recipe> recipes = Recipe.fromJsonList(fileContent);
+      await _recipeService.getAllRecipes();
       await _recipeService.importRecipes(recipes);
 
       ScaffoldMessenger.of(context).showSnackBar(
@@ -39,24 +45,41 @@ class _ImportExportPageState extends State<ImportExportPage> {
   }
 
   Future<void> _exportRecipes() async {
-    List<Recipe> recipes = await _recipeService.getAllRecipes();
-    String jsonContent = Recipe.toJsonList(recipes);
+    final permissionStatus = await Permission.manageExternalStorage.status;
+    if (permissionStatus.isDenied) {
+      // Ask for the permission for the first time
+      await Permission.manageExternalStorage.request();
 
-    // Use FilePicker to get the directory path from the user
-    String? directoryPath = await FilePicker.platform.getDirectoryPath();
+      // Sometimes the popup won't show after user presses deny
+      // so do the check once again but now go straight to appSettings
+      if (permissionStatus.isDenied) {
+        await openAppSettings();
+      }
+    } else if (permissionStatus.isPermanentlyDenied) {
+      // Here open app settings for user to manually enable permission in case
+      // permission was permanently denied
+      await Permission.manageExternalStorage.request();
+    } else {
+      // Do stuff that require permission here
+      List<Recipe> recipes = await _recipeService.getAllRecipes();
+      String jsonContent = Recipe.toJsonList(recipes);
 
-    if (directoryPath != null) {
-      String outputPath = '$directoryPath/recipes.json';
-      Uint8List bytes =
-          Uint8List.fromList(jsonContent.codeUnits); // Convert to Uint8List
-      File file = File(outputPath);
-      await file.writeAsBytes(bytes);
+      // Use FilePicker to get the directory path from the user
+      String? directoryPath = await FilePicker.platform.getDirectoryPath();
 
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-            content: Text(
-                'Recipes exported successfully! File saved at $outputPath')),
-      );
+      if (directoryPath != null) {
+        String outputPath = '$directoryPath/recipes.json';
+        Uint8List bytes =
+            Uint8List.fromList(jsonContent.codeUnits); // Convert to Uint8List
+        File file = File(outputPath);
+        await file.writeAsBytes(bytes);
+
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+              content: Text(
+                  'Recipes exported successfully! File saved at $outputPath')),
+        );
+      }
     }
   }
 
