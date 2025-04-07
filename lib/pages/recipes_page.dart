e52ae2e1f88662b878521909accd3e02aecde8fe
KrisB <kris.formal@gmail.com>
2025-04-07 14:57:38 +0300

    - Added the ability to select multiple recipes at once for delete / favourite operation

--- lib/pages/recipes_page.dart
+++ lib/pages/recipes_page.dart
@@ -14,6 +14,8 @@ class RecipesPage extends StatefulWidget {
 class _RecipesPageState extends State<RecipesPage> {
   final RecipeService _recipeService = RecipeService();
   List<Recipe> recipes = [];
+  bool isSelectionMode = false;
+  final Set<Recipe> selectedRecipes = {};
 
   @override
   void initState() {
@@ -37,38 +39,107 @@ class _RecipesPageState extends State<RecipesPage> {
     await _recipeService.deleteRecipe(recipe.id!);
   }
 
+  void _deleteSelectedRecipes() async {
+    for (var recipe in selectedRecipes) {
+      await _recipeService.deleteRecipe(recipe.id!);
+    }
+    setState(() {
+      recipes.removeWhere((r) => selectedRecipes.contains(r));
+      selectedRecipes.clear();
+      isSelectionMode = false;
+    });
+  }
+
+  void _toggleFavouriteSelectedRecipes() async {
+    for (var recipe in selectedRecipes) {
+      recipe.favourite = !recipe.favourite;
+      await _recipeService.updateRecipe(recipe);
+    }
+    setState(() {
+      selectedRecipes.clear();
+      isSelectionMode = false;
+    });
+  }
+
+  void _toggleSelectionMode() {
+    setState(() {
+      isSelectionMode = !isSelectionMode;
+      if (!isSelectionMode) {
+        selectedRecipes.clear();
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("My Recipes"),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.refresh),
-            onPressed: _loadRecipes, // refresh recipes
-          ),
-          IconButton(
-            icon: const Icon(Icons.add),
-            onPressed: () async {
-              final newRecipe = await Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (context) => const RecipeCreationPage()),
-              );
-              if (newRecipe != null) {
-                setState(() {
-                  recipes.add(newRecipe);
-                });
-                _saveRecipe(newRecipe);
-              }
-            },
-          ),
-        ],
+        title: Text(isSelectionMode
+            ? '${selectedRecipes.length} Selected'
+            : "My Recipes"),
+        actions: isSelectionMode
+            ? [
+                IconButton(
+                  icon: const Icon(Icons.favorite),
+                  onPressed: _toggleFavouriteSelectedRecipes,
+                ),
+                IconButton(
+                  icon: const Icon(Icons.delete),
+                  onPressed: _deleteSelectedRecipes,
+                ),
+                IconButton(
+                  icon: const Icon(Icons.close),
+                  onPressed: _toggleSelectionMode,
+                ),
+              ]
+            : [
+                IconButton(
+                  icon: const Icon(Icons.refresh),
+                  onPressed: _loadRecipes,
+                ),
+                IconButton(
+                  icon: const Icon(Icons.add),
+                  onPressed: () async {
+                    final newRecipe = await Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const RecipeCreationPage()),
+                    );
+                    if (newRecipe != null) {
+                      setState(() {
+                        recipes.add(newRecipe);
+                      });
+                      _saveRecipe(newRecipe);
+                    }
+                  },
+                ),
+              ],
       ),
       body: RecipeList(
         recipes,
+        isSelectionMode: isSelectionMode,
+        selectedRecipes: selectedRecipes,
         onDelete: _deleteRecipe,
         onRefresh: _loadRecipes,
+        onSelectionChange: (recipe, isSelected) {
+          setState(() {
+            if (isSelected) {
+              selectedRecipes.add(recipe);
+            } else {
+              selectedRecipes.remove(recipe);
+            }
+
+            // Exit selection mode if no recipes are selected
+            if (selectedRecipes.isEmpty) {
+              isSelectionMode = false;
+            }
+          });
+        },
+        onStartSelectionMode: () {
+          setState(() {
+            isSelectionMode = true;
+          });
+        },
       ),
     );
   }
--- lib/recipe_list.dart
+++ lib/recipe_list.dart
@@ -7,12 +7,25 @@ import 'dart:io';
 
 class RecipeList extends StatelessWidget {
   final List<Recipe> listItems;
+  final bool isSelectionMode;
+  final Set<Recipe> selectedRecipes;
   final Function(Recipe) onDelete;
   final Function() onRefresh;
+  final Function(Recipe, bool) onSelectionChange;
+  final Function()
+      onStartSelectionMode; // Added parameter for starting selection mode
   final RecipeService _recipeService = RecipeService();
 
-  RecipeList(this.listItems,
-      {super.key, required this.onDelete, required this.onRefresh});
+  RecipeList(
+    this.listItems, {
+    super.key,
+    required this.isSelectionMode,
+    required this.selectedRecipes,
+    required this.onDelete,
+    required this.onRefresh,
+    required this.onSelectionChange,
+    required this.onStartSelectionMode, // Initialize it in the constructor
+  });
 
   void _toggleFavourite(Recipe recipe) async {
     recipe.favourite = !recipe.favourite;
@@ -26,82 +39,105 @@ class RecipeList extends StatelessWidget {
       itemCount: listItems.length,
       itemBuilder: (context, index) {
         var recipe = listItems[index];
-        return Card(
-          child: Row(
-            children: <Widget>[
-              Expanded(
-                child: ListTile(
-                  title: Text(recipe.name),
-                  subtitle:
-                      Text(recipe.description ?? 'No description available'),
-                  leading: recipe.imagePath != null
-                      ? Image.file(
-                          File(recipe.imagePath!),
-                          fit: BoxFit.cover,
-                          width: 50,
-                          height: 50,
-                        )
-                      : const Icon(Icons.image_not_supported),
-                  trailing: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      if (recipe.favourite)
-                        const Icon(
-                          Icons.favorite,
-                          color: Colors.red,
-                        ),
-                      PopupMenuButton<String>(
-                        onSelected: (String value) {
-                          if (value == 'Favourite') {
-                            _toggleFavourite(recipe);
-                          } else if (value == 'Edit') {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => RecipeEditPage(
-                                  recipe: recipe,
-                                  onSave: (updatedRecipe) {
-                                    listItems[index] =
-                                        updatedRecipe; // Update the recipe in the list
-                                    onRefresh(); // Refresh the list
-                                  },
-                                ),
-                              ),
-                            );
-                          } else if (value == 'Delete') {
-                            onDelete(recipe);
-                          }
-                        },
-                        itemBuilder: (context) => [
-                          PopupMenuItem(
-                            value: 'Favourite',
-                            child: Text(
-                                recipe.favourite ? "Unfavourite" : "Favourite"),
-                          ),
-                          const PopupMenuItem(
-                            value: 'Edit',
-                            child: Text("Edit"),
-                          ),
-                          const PopupMenuItem(
-                            value: 'Delete',
-                            child: Text("Delete"),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) =>
-                            SelectedRecipePage(recipe: recipe),
-                      ),
-                    ).then((_) => onRefresh());
-                  },
+        final isSelected = selectedRecipes.contains(recipe);
+
+        return GestureDetector(
+          onLongPress: () {
+            if (!isSelectionMode) {
+              onStartSelectionMode(); // Trigger selection mode
+              onSelectionChange(recipe, true); // Select the recipe
+            }
+          },
+          onTap: () {
+            if (isSelectionMode) {
+              onSelectionChange(recipe, !isSelected); // Toggle selection
+            } else {
+              // Navigate to recipe details if not in selection mode
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => SelectedRecipePage(recipe: recipe),
                 ),
-              ),
-            ],
+              ).then((_) => onRefresh());
+            }
+          },
+          child: Card(
+            child: Row(
+              children: <Widget>[
+                if (isSelectionMode)
+                  Checkbox(
+                    value: isSelected,
+                    onChanged: (value) {
+                      onSelectionChange(recipe, value ?? false);
+                    },
+                  ),
+                Expanded(
+                  child: ListTile(
+                    title: Text(recipe.name),
+                    subtitle:
+                        Text(recipe.description ?? 'No description available'),
+                    leading: recipe.imagePath != null
+                        ? Image.file(
+                            File(recipe.imagePath!),
+                            fit: BoxFit.cover,
+                            width: 50,
+                            height: 50,
+                          )
+                        : const Icon(Icons.image_not_supported),
+                    trailing: isSelectionMode
+                        ? null
+                        : Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: <Widget>[
+                              if (recipe.favourite)
+                                const Icon(
+                                  Icons.favorite,
+                                  color: Colors.red,
+                                ),
+                              PopupMenuButton<String>(
+                                onSelected: (String value) {
+                                  if (value == 'Favourite') {
+                                    _toggleFavourite(recipe);
+                                  } else if (value == 'Edit') {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context) => RecipeEditPage(
+                                          recipe: recipe,
+                                          onSave: (updatedRecipe) {
+                                            listItems[index] = updatedRecipe;
+                                            onRefresh();
+                                          },
+                                        ),
+                                      ),
+                                    );
+                                  } else if (value == 'Delete') {
+                                    onDelete(recipe);
+                                  }
+                                },
+                                itemBuilder: (context) => [
+                                  PopupMenuItem(
+                                    value: 'Favourite',
+                                    child: Text(recipe.favourite
+                                        ? "Unfavourite"
+                                        : "Favourite"),
+                                  ),
+                                  const PopupMenuItem(
+                                    value: 'Edit',
+                                    child: Text("Edit"),
+                                  ),
+                                  const PopupMenuItem(
+                                    value: 'Delete',
+                                    child: Text("Delete"),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                  ),
+                ),
+              ],
+            ),
           ),
         );
       },
